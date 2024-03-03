a3b63410cbf1293b7833962f1b6bb49ce4679487
Sean Lyons <lyons.sean5@gmail.com>
2024-03-03 14:22:41 -0500

    add TextWithIcon for uses

---
+++ src/lib/TextWithIcon.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let text = "";
+    export let image = "";
+</script>
+
+<div class="flex w-fit items-center gap-2 rounded-md bg-slate-700 px-1 py-0.5">
+    <img src={image} alt="icon" class="size-4" />
+    <p class="whitespace-nowrap">{text}</p>
+</div>
--- src/routes/uses/+page.svelte
+++ src/routes/uses/+page.svelte
@@ -1,18 +1,30 @@
+<script>
+    import TextWithIcon from "$lib/TextWithIcon.svelte";
+</script>
+
 <div class="flex flex-col">
     <div class="flex justify-center"><h1>uses</h1></div>
     <img src="/uses computer.webp" alt="" />
-    <div class="flex flex-col">
-        <div><h1>category 1</h1></div>
-        <div class="grid grid-flow-col gap-4">
-            <div><p>tech 1</p></div>
-            <div><p>tech 2</p></div>
-            <div><p>tech 3</p></div>
+    <div class="flex flex-col gap-4">
+        <div class="flex">
+            <div class="w-1/2"><h1>category 1</h1></div>
+            <div class="flex flex-wrap gap-4">
+                <TextWithIcon text="tmux" image="/icons/tmux.ico" />
+                <TextWithIcon text="Visual Studio Code" image="/icons/vscode.ico" />
+                <TextWithIcon text="Visual Studio Code" image="/icons/vscode.ico" />
+                <TextWithIcon text="Visual Studio Code" image="/icons/vscode.ico" />
+                <TextWithIcon text="Visual Studio Code" image="/icons/vscode.ico" />
+            </div>
         </div>
-        <div><h1>category 2</h1></div>
-        <div class="grid grid-flow-col gap-4">
-            <div><p>tech 4</p></div>
-            <div><p>tech 5</p></div>
-            <div><p>tech 6</p></div>
+        <div class="flex">
+            <div class="w-1/2"><h1>category 2</h1></div>
+            <div class="flex flex-wrap gap-4">
+                <TextWithIcon text="tmux" image="/icons/tmux.ico" />
+                <TextWithIcon text="Visual Studio Code" image="/icons/vscode.ico" />
+                <TextWithIcon text="Visual Studio Code" image="/icons/vscode.ico" />
+                <TextWithIcon text="Visual Studio Code" image="/icons/vscode.ico" />
+                <TextWithIcon text="Visual Studio Code" image="/icons/vscode.ico" />
+            </div>
         </div>
     </div>
 </div>

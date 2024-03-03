97178d666fe83d5e8abb4efcd26002d630f36cc1
Sean Lyons <lyons.sean5@gmail.com>
2024-03-03 12:21:27 -0500

    uses placeholders

--- src/routes/uses/+page.svelte
+++ src/routes/uses/+page.svelte
@@ -1,1 +1,18 @@
-<h1>uses</h1>
+<div class="flex flex-col">
+    <div class="flex justify-center"><h1>uses</h1></div>
+    <img src="/uses computer.webp" alt="" />
+    <div class="flex flex-col">
+        <div><h1>category 1</h1></div>
+        <div class="grid grid-flow-col gap-4">
+            <div><p>tech 1</p></div>
+            <div><p>tech 2</p></div>
+            <div><p>tech 3</p></div>
+        </div>
+        <div><h1>category 2</h1></div>
+        <div class="grid grid-flow-col gap-4">
+            <div><p>tech 4</p></div>
+            <div><p>tech 5</p></div>
+            <div><p>tech 6</p></div>
+        </div>
+    </div>
+</div>
